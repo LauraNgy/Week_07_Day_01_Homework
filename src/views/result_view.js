90559703cf56ebaa610b3209163cc3c8cd949135
Laura Nagy <ngylaura@gmail.com>
2018-09-10 16:01:33 +0100

    adds a handle boolean method to result_view

--- src/views/result_view.js
+++ src/views/result_view.js
@@ -2,4 +2,14 @@ const ResultView = function () {
 
 };
 
+ResultView.prototype.handleResult = function (result) {
+  const displayElement = document.querySelector('#result');
+  if (result === true) {
+    displayElement.textContent = "This number is prime.";
+  }
+  else {
+    displayElement.textContent = "This number is not prime."
+  }
+};
+
 modules.exports = ResultView;
