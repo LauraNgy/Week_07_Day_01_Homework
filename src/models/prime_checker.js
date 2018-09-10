f4681c9783884c6d8855bc9f7e8b58087612dcd8
Laura Nagy <ngylaura@gmail.com>
2018-09-10 15:53:37 +0100

    adds pub_sub helper and prime_checker logic

--- src/models/prime_checker.js
+++ src/models/prime_checker.js
@@ -1,5 +1,22 @@
+const PubSub = require('../helpers/pub_sub.js');
+
 const PrimeChecker = function () {
 
+};
+
+PrimeChecker.prototype.checkPrime = function (number) {
+  let result = false;
+  if (number > 2) {
+    let divCount = 2;
+    for (let i = 2; i < number; i++) {
+      if (number % i === 0) {
+        divCount++;
+      }
+    }
+    if (divCount > 2) {
+      result = true;
+    }
+  }
 };
 
 module.exports = PrimeChecker;
