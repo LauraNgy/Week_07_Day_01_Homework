0f7c73556068c0ad430f8c70df7b0a0a39ac6584
Laura Nagy <ngylaura@gmail.com>
2018-09-10 15:50:38 +0100

    adds form_view

--- src/views/form_view.js
+++ src/views/form_view.js
@@ -1,5 +1,15 @@
+const PubSub = require('../helpers/pub_sub.js');
+
 const FormView = function () {
 
+};
+
+FormView.prototype.bindEvents = function () {
+  const formInput = document.querySelector('#prime-checker-form');
+  formInput.addEventListener('submit', (event) => {
+    const number = event.target.number.value;
+    PubSub.publish('FormView:number-submit', number);
+  });
 };
 
 modules.exports = FormView;
